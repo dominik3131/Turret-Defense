91acc4c67c8c64497d4d07fe1c9f9e38c2dca9df
JHinge <zawiasa.jakub@gmail.com>
2019-11-16 13:56:47 +0100

    proper object placing on map node

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -8,14 +8,16 @@ public class Node : MonoBehaviour
     
     void OnMouseDown()
     {
-        Debug.Log("welcome");
         if (turret != null)
         {
             Debug.Log("cant build");
             return;
         }
         GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
-        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
-        Debug.Log("placed");
+        float height = turretToBuild.transform.lossyScale.y/2;
+        Vector3 temp = new Vector3(transform.position.x, transform.position.y +1+ height, transform.position.z);
+     
+        turret = (GameObject)Instantiate(turretToBuild, temp, transform.rotation);
+        Debug.Log("Turret placed");
     }
 }
