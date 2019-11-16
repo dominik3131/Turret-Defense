7ccc2374db110e3d19de945c40100fac4150f9c3
JHinge <zawiasa.jakub@gmail.com>
2019-11-16 13:36:30 +0100

    turret spawner

---
+++ Assets/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private GameObject turret;
+    
+    void OnMouseDown()
+    {
+        Debug.Log("welcome");
+        if (turret != null)
+        {
+            Debug.Log("cant build");
+            return;
+        }
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
+        Debug.Log("placed");
+    }
+}
