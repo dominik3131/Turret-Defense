1598b00b51d90098ff55620493bc36ebb2e5794f
Marax97 <44068049+Marax97@users.noreply.github.com>
2019-11-20 22:01:34 +0100

    Added target object

---
+++ Assets/Scripts/TargetObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetObject : MonoBehaviour
+{
+    public float rangeOfDestroy = 0.5f;
+    public int health;
+
+    void Start()
+    {
+        health = 100;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            showLossingScreen();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            health -= collision.gameObject.GetComponent<Enemy>().damage;
+        }
+    }
+
+    private void showLossingScreen()
+    {
+        Destroy(gameObject);
+    }
+}
