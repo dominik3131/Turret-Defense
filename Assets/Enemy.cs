6feb968a0bf30f4247b676183ed29a2daccbb192
p.wolski@polsource.com <pa-wo97@o2.pl>
2019-11-20 17:34:27 +0100

    Rotate enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 2f;
-
+    public float smooth = 1f;
     private Transform target;
 
     private int wavePointIndex = 0;
@@ -20,7 +20,10 @@ public class Enemy : MonoBehaviour
         {
             Vector3 direction = target.position - transform.position;
             transform.Translate(direction.normalized * 3 * Time.deltaTime, Space.World);
-
+            var lookPos = target.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * smooth);
             if (Vector3.Distance(transform.position, target.position) <= 0.4f)
             {
                 getNextWaypoint();
