b5f0cd84d3de032c4723478d03bd10cf8b88f46e
Dominik <rakdominikrak@gmail.com>
2020-01-15 16:42:13 +0100

    fast forward button

---
+++ Assets/FastForwardManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FastForwardManager : MonoBehaviour
+{
+    public Button fastForwardButton;
+    private bool fastForwarded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fastForwardButton.onClick.AddListener(FastForward);
+    }
+
+    void FastForward()
+    {
+        if ( fastForwarded )
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 2;
+        }
+        fastForwarded = !fastForwarded;
+    }
+}
